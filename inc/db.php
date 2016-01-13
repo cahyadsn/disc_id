a7a2d9935f471248676d6dedccbf65d3e9bf60c9
Cahya DSN <cahyadsn@gmail.com>
2016-01-13 18:16:05 +0700

    Create inc/db.php

---
+++ inc/db.php
@@ -0,0 +1,8 @@
+<?php
+  //-- database configuration
+  $dbhost='localhost';
+  $dbuser='root';
+  $dbpass='';
+  $dbname='disc';
+  //-- database connection
+  $db=new mysqli($dbhost,$dbuser,$dbpass,$dbname);
